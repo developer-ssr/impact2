43700d751a44759a31b6fb057e39b0bf5576834c
jannarmarvega <110706609+jannarmarvega@users.noreply.github.com>
2024-08-05 13:36:00 +0800

    update main sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
-
+import "../css/style.css";
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
